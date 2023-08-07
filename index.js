093495cef5dd523a122fa3a1efe1c68e90874669
arman <arman.erkoyan.goya@gmail.com>
2023-08-07 23:16:38 +0400

    error 'document is not a difaind'

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
+
+
+
 var arr = [2, 3, 6, 3, 9, 7]
 var arr1 = [2, 3, 6, 32, 9, 71]
 var arr2 = [2, 3, 6, 34, 90, 7]
@@ -14,18 +17,15 @@ a(arr)
 a(arr1)
 a(arr2)
 
-console.log(bc);
-function b() {
-    var bc = 0
-}
-b()
 
 
 let ops = 0;
 function color(elem) {
     ops++;
-    console.log(typeof ops,ops);
+    console.log(typeof ops, ops);
     elem.innerHTML = ops
     document.getElementById('input').innerHTML = "Armancho";
-  
+    elem.style.color = "red"
+
 }
+
