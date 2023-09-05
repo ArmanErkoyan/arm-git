9a8e6d904184ca1b285ccbdf7111947447301ed2
arman_goya <arman.erkoyan.goya@gmail.com>
2023-09-05 12:42:28 +0400

    Leedcod tasks

--- index.js
+++ index.js
@@ -1,7 +1,147 @@
 
 
-const element = document.getElementById("gen_div1");
-let tegsAdd = document.createElement("div")
-function add(){
-    tegsAdd.appendChild(element)
+// // var element = document.getElementById("gen_div1");
+// // let tegsAdd = document.createElement("div")
+// // function add() {
+// //     tegsAdd.appendChild(element)
+// // }
+// var a = 5
+
+// switch (a) {
+//     case "4":
+//         console.log("a = 4");
+//         break
+//     case "6":
+//         console.log("a = 6");
+//         break
+//     case 5:
+//         console.log("a = 5");
+//         break
+//     default:
+//         console.log("asss");
+// }
+// let s= 0
+// setInterval(function ab(){
+
+// },1500)
+// var asus = setInterval(acba,500)
+
+// function acba(){
+//     console.log(s)
+//     s++
+//     if (s==3) {
+//         clearInterval(asus)
+//     }
+// }
+
+// setTimeout( function(){
+//     console.log("Arman");
+// },1500)
+
+
+// let arr = [8, 9, 7, 89, 59, 46]
+
+// console.log(typeof arr[3]);
+// console.log(typeof arr.sort().join(""));
+
+// var a
+// console.log(a);
+// var a=5
+
+// const poi = [8, 9, 7, 89, 59, 46]
+// poi.sort(function(a,b){return a-b})
+// console.log(poi);
+
+// class parents {
+
+
+//     constructor(name,age,status){
+//         this.name = name;
+//         this.age = age;
+//         this.status = status;
+//     }
+
+
+//      info(){
+//         console.log("name is" + " " + this.name);
+//     };
+// }
+
+// var alex = new parents('Alex',23,true)
+// var jon = new parents('jon',27,false)
+
+
+// alex.info()
+
+// let arr = [...arr1, ...arr2]
+// let mid
+// let c = 0
+// arr.sort(function (a, b) { return a - b })
+
+// for (var i = 0; i < arr.length; i++) {
+//     c = arr[i] + c;
+// }
+// mid = c / i
+
+// mid = mid.toFixed(5)
+
+
+
+// return (mid);
+// let n;
+// console.log(n);
+// let count=1;
+// function asus() {
+//     if (n==undefined) {
+//         count=0;
+//         return
+//     }
+
+// while (n/10>10) {
+   
+//     count++
+//     n=n/10;
+//     console.log(n);
+  
+// }
+// }
+// asus()
+// console.log(count);
+// let num = [1,3,6,9,6,5,9,5]
+// let tex=[]
+// let num1
+// add=100
+// tex=num.find((v,i,a)=>{
+//     console.log(tex);
+//     return v>1
+
+// })
+// console.log(tex);
+// console.log(true+true);
+
+
+// let arr2=''
+// var myAtoi = function(s) {
+//     for(i=0;i<s.length;i++){
+//         console.log(s[i].charCodeAt());
+//         if (s[i].charCodeAt()<65 || s[i].charCodeAt()>122) {
+//             arr2+=s[i]
+//         }
+
+//     }
+//     console.log(arr2)
+//     return Number(arr2)
+// }
+// a="      -42"
+// var e = myAtoi(a)
+// console.log(e);
+// let cd = "z"
+// // console.log(cd.charCodeAt())
+// console.log(typeof "a");
+let k=1485654
+console.log(NaN%1);
+if(typeof k == NaN){
+    
+    console.log("asus");
 }
+undefined
